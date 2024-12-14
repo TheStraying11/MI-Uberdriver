b94e5e480e2904599f2880688af32228a620f9b0
TheStraying11 <jtrycroft@gmail.com>
2024-12-14 07:48:05 +0000

    im lazy

--- src/main/java/uk/studiolucia/uberdriver/mixins/SlotTypeMixin.java
+++ src/main/java/uk/studiolucia/uberdriver/mixins/SlotTypeMixin.java
@@ -1,5 +1,6 @@
 package uk.studiolucia.uberdriver.mixins;
 
+import aztech.modern_industrialization.MIItem;
 import aztech.modern_industrialization.MIText;
 import aztech.modern_industrialization.inventory.SlotGroup;
 import aztech.modern_industrialization.machines.guicomponents.SlotPanel.SlotType;
@@ -15,16 +16,8 @@ import java.util.function.Predicate;
 
 @Mixin(value = SlotType.class)
 public class SlotTypeMixin {
-/*
-    @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target="Laztech/modern_industrialization/machines/guicomponents/SlotPanel$SlotType;<init>(Laztech/modern_industrialization/inventory/SlotGroup;ILjava/util/function/Predicate;IILaztech/modern_industrialization/MIText;)V", ordinal = 3))
-    private static Predicate<ItemStack> modifyPredicate(ItemStack stack) {
-
-        return stack;
-    }
-*/
-
-    @WrapOperation(method = "<clinit>", at = @At(value = "INVOKE", target="aztech/modern_industrialization/machines/guicomponents/SlotPanel$SlotType.<init> (Ljava/lang/String;ILaztech/modern_industrialization/inventory/SlotGroup;ILjava/util/function/Predicate;IILaztech/modern_industrialization/MIText;)V", ordinal = 3))
-    private static SlotType SlotTypeWrap(SlotGroup group, int slotLimit, Predicate<ItemStack> insertionChecker, int u, int v, MIText tooltip, Operation<SlotType> original) {
-        return original.call(group, slotLimit, insertionChecker.or(stack -> stack.is(ModItems.UberdriveModule)), u, v, tooltip);
+    @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target="aztech/modern_industrialization/machines/guicomponents/SlotPanel$SlotType.<init> (Ljava/lang/String;ILaztech/modern_industrialization/inventory/SlotGroup;ILjava/util/function/Predicate;IILaztech/modern_industrialization/MIText;)V", ordinal = 3))
+    private static Predicate<ItemStack> modifyPredicate(Predicate<ItemStack> insertionChecker) {
+        return insertionChecker.or(stack -> stack.is(ModItems.UberdriveModule));
     }
 }
